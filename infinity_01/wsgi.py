5b97db67bc7bb0ae77217d8ef5097e1f5184a032
vishavant <officialvishavant@gmail.com>
2022-12-27 16:01:28 +0530

    Checking project pipeline

--- infinity_01/wsgi.py
+++ infinity_01/wsgi.py
@@ -20,3 +20,21 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'infinity_01.settings')
 
 application = get_wsgi_application()
+
+
+
+# To use your own django app use code like this:
+# import os
+# import sys
+#
+## assuming your django settings file is at '/home/vishavant/mysite/mysite/settings.py'
+## and your manage.py is is at '/home/vishavant/mysite/manage.py'
+#path = '/home/vishavant/mysite'
+#if path not in sys.path:
+#    sys.path.append(path)
+#
+#os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+#
+## then:
+#from django.core.wsgi import get_wsgi_application
+#application = get_wsgi_application()
